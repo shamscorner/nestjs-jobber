0a4cb63b14c99ec2a04c460cab0f665625338326
Shamim <hossains159@gmail.com>
2025-03-13 19:52:11 +0600

    feat: integrate GraphQL with job execution capabilities and add JobsResolver

--- apps/jobber-jobs/src/app/app.module.ts
+++ apps/jobber-jobs/src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { JobsModule } from './jobs/jobs.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [ConfigModule, JobsModule],
+  imports: [
+    ConfigModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: {
+        settings: {
+          'request.credentials': 'include',
+        },
+      },
+      autoSchemaFile: true,
+    }),
+    JobsModule,
+  ],
   controllers: [],
   providers: [],
 })
--- apps/jobber-jobs/src/app/jobs/abstract.job.ts
+++ apps/jobber-jobs/src/app/jobs/abstract.job.ts
@@ -1,1 +1,5 @@
-export abstract class AbstractJob {}
+export abstract class AbstractJob {
+  execute() {
+    console.log('Executing job...');
+  }
+}
