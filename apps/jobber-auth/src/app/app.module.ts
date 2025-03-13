c85b97b5e39a9a6d3eb67ed8af99ca638a6decef
Shamim <hossains159@gmail.com>
2025-03-13 15:53:35 +0600

    feat: add authentication module with JWT support and configuration

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ConfigModule } from '@nestjs/config';
       autoSchemaFile: true,
     }),
     UsersModule,
+    AuthModule,
   ],
   providers: [],
 })
